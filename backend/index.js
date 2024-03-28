44902759bce81b435d699eea2c954524056ee7a1
Tushar458 <83863028+Tushar458@users.noreply.github.com>
2024-03-28 19:58:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,12 @@ const app = express();
 // Enable CORS middleware
 app.use(cors(
   {
-    origin: ["https://smart-brain-api-eight.vercel.app"],
-    methods: ["POST","GET"],
+    origin: {"https://smart-brain-api-eight.vercel.app"},
+    methods: {"POST", "GET", "DELETE", "PUT"},
     credentials: true
   }
 ));
+app.use(express.json({ limit: "25mb" }));
 app.get("/",(req,res)=>
 {
   res.json("Wait what ?");
